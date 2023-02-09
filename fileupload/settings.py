80ef86f068b55ccecdce5aeeff944bf13b10965f
Victor Gomez <it.desarrollo02@parma.gt>
2023-02-09 09:20:06 -0600

    [ADD] APP Settings
    [ADD] APP Settings

--- fileupload/settings.py
+++ fileupload/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework.authtoken',
+    'uploadapp',
     'corsheaders'
 ]
 
@@ -169,10 +170,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
-
 #STATIC_ROOT = 'static'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
